be8e49c721d491d3e9cd5913c52328ff531e88d0
Artem Garanskyy <art.garanskiy@gmail.com>
2024-12-06 06:35:23 +0800

    comments added  meal item trait  widget

--- lib/widgets/meal_item_trait.dart
+++ lib/widgets/meal_item_trait.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
+//meal item trait stateless class
 class MealItemTrait extends StatelessWidget {
+
+  //constructor for the meal item trait
   const MealItemTrait({
     super.key,
     required this.icon,
@@ -11,14 +14,19 @@ class MealItemTrait extends StatelessWidget {
   final String label;
 
   @override
+
+  //build method for the meal item trait
   Widget build(BuildContext context) {
+    
     return Row(children: [
+      //icon for the meal item trait
       Icon(
         icon,
         size: 17,
         color: Colors.white,
       ),
       const SizedBox(width: 6),
+      //text widget for the meal item trait
       Text(
         label,
         style: const TextStyle(
