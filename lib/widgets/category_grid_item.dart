6df744561309cc4f9bd739ffefd0a779c456e796
Artem Garanskyy <art.garanskiy@gmail.com>
2024-12-06 06:34:48 +0800

    comments added to category grid item widget

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:meals/models/category.dart';
 
+//category grid item widget stateless class
 class CategoryGridItem extends StatelessWidget {
   const CategoryGridItem({
     super.key,
@@ -14,14 +15,26 @@ class CategoryGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    //return an inkwell widget
     return InkWell(
+      //on tap event handler
       onTap: onSelectCategory,
+
+      //splash color
       splashColor: Theme.of(context).primaryColor,
+
+      //border radius
       borderRadius: BorderRadius.circular(16),
+
       child: Container(
+        //padding for the container
         padding: const EdgeInsets.all(16),
+        //decoration for the container
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(16),
+
+            //gradient for the container
             gradient: LinearGradient(
               colors: [
                 category.color.withOpacity(0.55),
@@ -30,6 +43,8 @@ class CategoryGridItem extends StatelessWidget {
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
             )),
+            
+            //text widget for the category title
         child: Text(
           category.title,
           style: Theme.of(context).textTheme.titleLarge!.copyWith(
