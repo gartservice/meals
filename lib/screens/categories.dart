99fa77a46538d233771ea62ed4a5b8dbd8b7259f
Artem Garanskyy <art.garanskiy@gmail.com>
2024-12-06 06:03:17 +0800

    comments added to categories screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -6,23 +6,36 @@ import 'package:meals/widgets/category_grid_item.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/models/category.dart';
 
+//categories screen widget
 class CategoriesScreen extends StatelessWidget {
+  
+  //constructor for the categories screen
   const CategoriesScreen({
     super.key,
     required this.onToggleFavorite,
     required this.availableMeals,
   });
 
+  //final fields for the class constructor
   final void Function(Meal meal) onToggleFavorite;
   final List<Meal> availableMeals;
 
+  //method to select a category
   void _selectCategory(BuildContext context, Category category) {
+
+    //filter the meals based on the selected category
     final filteredMeals = availableMeals
+        //return a new list of meals that contain the selected category
         .where((meal) => meal.categories.contains(category.id))
+
+        //convert the iterable to a list
         .toList();
 
+    //navigate to the meals screen
     Navigator.of(context).push(
       MaterialPageRoute(
+
+        //pass the selected category and filtered meals to the meals screen
         builder: (ctx) => MealsScreen(
           title: category.title,
           meals: filteredMeals,
@@ -34,8 +47,12 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //return a grid view of the available categories
     return GridView(
+      //padding for the grid view
       padding: const EdgeInsets.all(24),
+
+      //grid view delegate
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
         childAspectRatio: 3 / 2,
@@ -44,10 +61,14 @@ class CategoriesScreen extends StatelessWidget {
       ),
       children: [
         // availableCategories.map((category) => CategoryGridItem(category: category)).toList()
+        //loop through the available categories
         for (final category in availableCategories)
+
+          //return a category grid item for each category
           CategoryGridItem(
             category: category,
             onSelectCategory: () {
+              //select the category when it is tapped
               _selectCategory(context, category);
             },
           )
